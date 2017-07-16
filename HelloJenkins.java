5071e328b96ef546f16f7b46903bfa38b4244844
monisvn <monisvn@gmail.com>
2017-07-16 19:19:09 +0530

    added hello jenkins

---
+++ HelloJenkins.java
@@ -0,0 +1,12 @@
+package loginFlight;
+
+public class HelloJenkins {
+
+	public static void main(String[] args) {
+	for(int i=1;i<=5;i++){
+		System.out.println("Jenkins world: "+ i);
+	}
+
+	}
+
+}

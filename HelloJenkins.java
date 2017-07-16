bd98066836a90585f511dde91bc57d775c414157
monisvn <monisvn@gmail.com>
2017-07-16 19:35:17 +0530

    modified HelloJenkins

--- HelloJenkins.java
+++ HelloJenkins.java
@@ -3,7 +3,7 @@ package loginFlight;
 public class HelloJenkins {
 
 	public static void main(String[] args) {
-	for(int i=1;i<=5;i++){
+	for(int i=1;i<=6;i++){
 		System.out.println("Jenkins world: "+ i);
 	}
 
